c9462808f4254b5ead0a82c1e90062afa5a67580
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2025-04-09 01:37:40 +0600

    Final

---
+++ FindJessia.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string word;
+    bool tt=false;
+
+    
+    while (cin >> word) {
+       
+        if (word == "Jessica")
+        {
+            tt=true;
+        }
+    }
+
+
+    if (tt)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
