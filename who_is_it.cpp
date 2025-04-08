d741a29449fc04f3fb8f95c5dabfd5af51e49632
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2025-04-08 21:54:09 +0600

    Mid

---
+++ who_is_it.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Student {
+public:
+    int id;
+    string name;
+    string section;
+    int marks;
+    Student(int id,string name,string section,int marks):id(id),name(name),section(section),marks(marks){}
+};
+int main(){
+    int T;
+    cin>>T;
+    for(int testCase=1; testCase<=T; testCase++){
+        int highestMarks=-1, highestID=INT_MAX;
+        Student* topStudent=nullptr;
+        for (int i=0; i<3; i++)
+        {
+            int id,marks;
+            string name,section;
+            cin >>id>>name>>section>>marks;
+            Student* currentStudent=new Student(id,name,section,marks);
+            if(currentStudent->marks>highestMarks || (currentStudent->marks==highestMarks && currentStudent->id<highestID))
+            {
+                highestMarks=currentStudent->marks;
+                highestID=currentStudent->id;
+                topStudent=currentStudent;
+            } else{
+                delete currentStudent;
+            }
+        }
+        cout<<topStudent->id<<" " <<topStudent->name<< " "<<topStudent->section<<" "<<topStudent->marks<<endl;
+        delete topStudent; 
+    }
+    return 0;
+}
