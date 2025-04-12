a5c643e351acd2e562c241b5ba0f85db887d8ad3
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2025-04-12 18:50:13 +0600

    Assignment 2

---
+++ IsItValid.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+class IsItValid{
+public:
+    bool Is_It_Valid(string s){
+        stack<char>st;
+        for(char c:s){
+            if(c=='0'){
+                if(!st.empty() && st.top()=='1')
+                {
+                    st.pop();
+                }
+                else{
+                    st.push(c);
+                }
+            }
+            else if(c=='1'){
+                if(!st.empty() && st.top()=='0')
+                {
+                    st.pop();
+                }
+                else{
+                    st.push(c);
+                }
+            }
+        }
+        return st.empty();
+    }
+};
+int main(){
+    int T;
+    cin>>T;
+    IsItValid valdt;
+    for(int i=0; i<T; ++i){
+        string s;
+        cin>>s;
+        if(valdt.Is_It_Valid(s)){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
