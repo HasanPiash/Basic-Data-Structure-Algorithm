35e883ad21cb1caa56e41ff2ef2876335fc1af36
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2025-04-20 00:02:59 +0600

    Mid Term

---
+++ RemoveDuplicateAgain.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+class RemoveDuplicate{
+public:
+    list<int>deleteDuplicates(list<int>& dplct)
+    {
+        dplct.sort(); 
+        dplct.unique(); 
+        return dplct;
+    }
+    void p_list(list<int>&dplct){
+        for(int val:dplct)
+        {
+            cout<<val<<" ";
+        }
+        cout<<endl;
+    }
+};
+int main(){
+   RemoveDuplicate rmvDuplicate;
+    list<int>dplct;
+    int val;
+    while(cin>>val && val!=-1)
+    {
+        dplct.push_back(val);
+    }
+    list<int>tmp=rmvDuplicate.deleteDuplicates(dplct);
+    rmvDuplicate.p_list(tmp);
+    return 0;
+}
