a11d886447439940166df03d4ec86f7f1cc54a8f
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-06-04 20:03:06 +0600

    036 adjacency list to edge

--- 036_adjacency_list_to_edge.cpp
+++ 036_adjacency_list_to_edge.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>  
 using namespace std;
 class Edge
 {
@@ -11,17 +11,19 @@ public:
         this->w = w;
     }
 };
+ 
 int main()
 {
     int n, e;
     cin >> n >> e;
-    vector<pair<int, int>> v[n];
+    vector<pair<int, int>> v[n]; 
     while (e--)
     {
         int a, b, c;
         cin >> a >> b >> c;
         v[a].push_back({b, c});
     }
+    
     vector<Edge> edgeList;
     for (int i = 0; i < n; i++)
     {
@@ -32,9 +34,10 @@ int main()
             edgeList.push_back(Edge(i, childNode, cost));
         }
     }
+    
     for (Edge edge : edgeList)
     {
         cout << edge.u << " " << edge.v << " " << edge.w << endl;
     }
     return 0;
-}
+} 
