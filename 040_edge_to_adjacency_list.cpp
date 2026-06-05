4b8d4b65de9da3dd712d3bd8c8eddaef0da681ce
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-06-05 21:51:25 +0600

    040 edge to adjacency list

--- 040_edge_to_adjacency_list.cpp
+++ 040_edge_to_adjacency_list.cpp
@@ -5,12 +5,14 @@ int main()
     int n, e;
     cin >> n >> e;
     vector<pair<int, int>> v[n];
+    
     while (e--)
     {
         int a, b, c;
         cin >> a >> b >> c;
         v[a].push_back({b, c});
     }
+    
     for (int i = 0; i < n; i++)
     {
         cout << i << " -> ";
@@ -20,5 +22,5 @@ int main()
         }
         cout << endl;
     }
-    return 0;
+    return 0;   
 }
