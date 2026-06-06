6dd07cecab9a4267c38ccdcbabd5ad17dac1ee06
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-06-06 22:08:21 +0600

    045 Building Roads

--- 045_Building_Roads.cpp
+++ 045_Building_Roads.cpp
@@ -11,6 +11,7 @@ void dsu_initialize(int n)
         group_size[i] = 1;
     }
 }
+
 int dsu_find(int node)
 {
     if (parent[node] == -1)
@@ -19,6 +20,7 @@ int dsu_find(int node)
     parent[node] = leader;
     return leader;
 }
+
 void dsu_union_by_size(int node1, int node2)
 {
     int leaderA = dsu_find(node1);
@@ -28,13 +30,15 @@ void dsu_union_by_size(int node1, int node2)
         parent[leaderB] = leaderA;
         group_size[leaderA] += group_size[leaderB];
     }
+        
     else
     {
         parent[leaderA] = leaderB;
         group_size[leaderB] += group_size[leaderA];
     }
 }
-int main()
+ 
+int main() 
 {
     int n, e;
     cin >> n >> e;
@@ -50,6 +54,7 @@ int main()
             dsu_union_by_size(a, b);
         }
     }
+     
     vector<int> leader;
     for (int i = 1; i <= n; i++)
     {
