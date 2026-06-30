e095457e6c1e2969e3a0e228e18f7b6338294289
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-06-30 20:13:26 +0600

    Assignment : Array String

--- Array_String.cpp
+++ Array_String.cpp
@@ -1,14 +1,14 @@
-#include <iostream> 
+#include <iostream>
 #include <string.h>
 #include <string>
 using namespace std;
 int main()
 {
-    char s[100];
+    char s[100]; 
     int a;
     cin >> a;   
     getchar();
-    
+     
     // fgets(s, 100, stdin);
     cin.getline(s, 100);
     cout << a << endl;
