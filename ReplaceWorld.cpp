42868d714b4210b88d5d74a325ef784239eb34ed
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-07-08 21:25:40 +0600

    Assignment : Replace World

--- ReplaceWorld.cpp
+++ ReplaceWorld.cpp
@@ -1,36 +1,31 @@
-#include <iostream> 
+#include <iostream>
 #include <string>
-
 using namespace std;
-
 int main() {
     int T;
     cin >> T;
-
-    for (int i = 0; i < T; i++) {
+    for (int i = 0; i < T; i++)
+    {
         string S, X;
         cin >> S >> X;
-
         int a = X.size();
         int b = S.size();
-
-        for (int j = 0; j <= b -a; j++) {
+        for (int j = 0; j <= b -a; j++)
+        {
             bool found = true;
-            for (int k = 0; k < a; k++) {
-                if (S[j + k] != X[k]) {
+            for (int k = 0; k < a; k++)
+            {
+                if (S[j + k] != X[k])
+                {
                     found = false;
-    
                 }
             }
-
             if (found) {
                 S.replace(j, a, "#");
                 b = S.size();
             }
         }
-
         cout << S << endl;
     }
-
     return 0;
 }
