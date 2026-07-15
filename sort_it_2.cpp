f1d7a794ff967d5d15d103acbd99ffd8d8591e51
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-07-15 23:23:30 +0600

    Assignment : sort it 2

--- sort_it_2.cpp
+++ sort_it_2.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 using namespace std;
 int* sort_it(int size) {
     int* arr = new int[size];  
@@ -7,14 +7,15 @@ int* sort_it(int size) {
     }
     sort(arr, arr + size, greater<int>());
     return arr;  
-} 
+}
+
 int main() {
     int N;
     cin >> N;
-    int* sorted_arr = sort_it(N); 
+    int* sorted_arr = sort_it(N);
     for (int i = 0; i < N; i++) {
         cout << sorted_arr[i] << " ";
     }
-    delete[] sorted_arr;  
+    delete[] sorted_arr;
     return 0;
 }
