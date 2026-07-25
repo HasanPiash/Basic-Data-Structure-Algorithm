e4bd44f8e5c4fd4d1764efc2420daecbfb6ee47e
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-07-25 20:25:57 +0600

    Assignment : matrix to adjacency list 2

--- 038_matrix_to_adjacency_list_2.cpp
+++ 038_matrix_to_adjacency_list_2.cpp
@@ -1,11 +1,10 @@
-#include <bits/stdc++.h> 
-using namespace std;
+#include <bits/stdc++.h>
+using namespace std; 
 int main()
 {
     int n;
     cin >> n;
     int mat[n][n];
-    
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
@@ -23,7 +22,6 @@ int main()
             }
         } 
     }
-     
     for (int i = 0; i < n; i++)
     {
         cout << i << " -> ";
