250b35293379c698542c8ec69344d1a1844016bd
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-07-27 22:07:55 +0600

    Assignment : matrix to edge list

--- 041_matrix_to_edge_list.cpp
+++ 041_matrix_to_edge_list.cpp
@@ -1,11 +1,10 @@
 #include <bits/stdc++.h>
-using namespace std; 
+using namespace std;
 int main()
 {
     int n;
     cin >> n;
     int mat[n][n];
-    
     for (int i = 0; i < n; i++) 
     {
         for (int j = 0; j < n; j++)
@@ -23,7 +22,6 @@ int main()
                 edgeList.push_back({i, j});
             }
         }
-     
     for (pair<int, int> edge : edgeList)
     {
         cout << edge.first << " " << edge.second << endl;
