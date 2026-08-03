a3648c38501fafd3205b52ca865c928815242fac
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-08-03 20:24:50 +0600

    Assignment : factorial

--- 057_factorial.cpp
+++ 057_factorial.cpp
@@ -11,7 +11,7 @@ int fact(int n)
     int chotoFactorial = fact(n - 1);
     return chotoFactorial * n;
 }
- 
+
 int main()
 {
     int n;
@@ -21,8 +21,7 @@ int main()
     for (int i = 1; i <= n; i++)
     {
         ans *= i;
-    }   
-    
+    }
     cout << ans << endl;
     return 0;
 }
