3c3584a4b96224b23747d1767fe9eda9bd4cb7c5
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-08-05 20:07:51 +0600

    Assignment : fibonacci bottom up

--- 059_fibonacci_bottom_up.cpp
+++ 059_fibonacci_bottom_up.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-using namespace std; 
+using namespace std;
 int main()
 {
     int n;
@@ -7,12 +7,11 @@ int main()
     int a[n];
     a[0] = 0;
     a[1] = 1;
-    
     // O(N)
     for (int i = 2; i <= n; i++)
     {
         a[i] = a[i - 1] + a[i - 2];
     } 
     cout << a[n] << endl;  
-    return 0;
+    return 0; 
 }
