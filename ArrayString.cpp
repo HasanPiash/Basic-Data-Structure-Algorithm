e0017691c4e120ecde26289197298ee70383b420
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-08-08 23:12:20 +0600

    Assignment : ArrayString

--- ArrayString.cpp
+++ ArrayString.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 #include <string.h> 
-#include <string>  
+#include <string>
 using namespace std;
 int main()
 {
     char s[100];
     int a;
     cin >> a;
-    getchar();
-       
+    getchar();   
     // fgets(s, 100, stdin);
-    
     cin.getline(s, 100);
     cout << a << endl;
     cout << s << endl;
